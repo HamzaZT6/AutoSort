ebf6c8314889883c81abdb8a8dba821958bf628b
Hamza <itszaska777@gmail.com>
2025-12-16 00:01:56 +0100

    Enhance file sorting tool with UI and bug fixes
    Refactor file organization tool to improve UI and fix typos.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import os 
 import time 
-import rich
+import shutil
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import Progress
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def render_ui():
+    global console
+    console = Console()
+    main_panel = Panel(
+        "[bold]Do You Want To AutoSort Your Donlowdes Folder ? ([green]y[/green]/[red]n[/red]):[/bold]",
+        title="AutoSort",
+        border_style="yellow"
+    )
+
+    console.print(main_panel)
 
 def split_files():
-    files = os.listdir("Donwloads")
+    files = os.listdir("Downloads")
 
     all_ext = [
         # Images
@@ -18,50 +35,76 @@ def split_files():
         # Documents
         [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".txt", ".csv", ".md", ".rtf"]
     ]
+    with Progress() as progress:
+        task = progress.add_task("Moving files", total=len(files))
+        # Images
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext in all_ext[0]:
+                shutil.move(f"Downloads/{f}", f"Downloads/Images/{f}")
+                progress.update(task, advance=1)
+        print("[+] - Images Moved")
 
-    for file in files:
-        name, ext = os.path.splitext(file)
+        # Videos
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext in all_ext[1]:
+                shutil.move(f"Downloads/{f}", f"Downloads/Videos/{f}")
+                progress.update(task, advance=1)
+        print("[+] - Videos Moved")
 
-        if ext in all_ext:
-            print(file)
-    
+        # Music
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext in all_ext[2]:
+                shutil.move(f"Downloads/{f}", f"Downloads/Music/{f}")
+                progress.update(task, advance=1)
+        print("[+] - Musics Moved")
 
-
-def create_folders():
-    print("[Tool]: We Are Now On The First Step")
-    print("[Tool]: I Will Create 4 Folders (Images, Videos, Musics, Documents) In Downlode Folder")
-    choice = input("[Tool]: Can I Create Them? (y/n): ").lower()
-
-    if choice == "y" or choice == "yes":
+        # Documents
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext in all_ext[3]:
+                shutil.move(f"Downloads/{f}", f"Downloads/Documents/{f}")
+                progress.update(task, advance=1)
+        print("[+] - Documents Moved")
+        
+def create_folders():   
+    with Progress() as progress:
+        task = progress.add_task("Creating Folders", total=4)
         if not os.path.exists("Downloads/Images"):
             os.makedirs("Downloads/Images")
             print("[+] - Images Folder Created")
+            progress.update(task, advance=1)
+            time.sleep(0.5)
         if not os.path.exists("Downloads/Videos"):
             os.makedirs("Downloads/Videos")
             print("[+] - Videos Folder Created")
+            progress.update(task, advance=1)
+            time.sleep(0.5)
         if not os.path.exists("Downloads/Music"):
             os.makedirs("Downloads/Music")
             print("[+] - Music Folder Created")
+            progress.update(task, advance=1)
+            time.sleep(0.5)
         if not os.path.exists("Downloads/Documents"):
             os.makedirs("Downloads/Documents")
             print("[+] - Documents Folder Created")
-
-    elif choice == "n" or choice == "no":
-        print("[Tool]: Why Even You Opend Me ???")
-    else:
-        print("[Tool]: ... Try With (yes or no)")
-    
+            progress.update(task, advance=1)
+            time.sleep(0.5)
 
 def main():
-    print("[Tool]: Welcome, Im Here To Organiez Your Files")
-    choice = input("[Tool]: Do You Want Me To Organiez Your Donlowdes Folder ? (y/n): ").lower()
-    
-    if choice == "y" or choice == "yes":
-        create_folders()
-        split_files()
-    elif choice == "n" or choice == "no":
-        print("[Tool]: What a wierdo")
-    else:
-        print(f"[Tool]: For Real Bro ? You had One Job, Try (Yes OR No)")
+    while True:
+        clear_screen()
+        render_ui()
+        choice = input("[Input]: > ").lower()
+        
+        if choice == "y" or choice == "yes":
+            create_folders()
+            split_files()
+        elif choice == "n" or choice == "no":
+            print("[Tool]: Bye, Have A Good Day")
+        else:
+            print(f"[Tool]: For Real Bro ?, Try (Yes OR No)")
 
 main()
