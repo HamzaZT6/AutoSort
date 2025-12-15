ac0498ce0afc6e1c5cae19e383a7c15404fe77d1
Hamza <itszaska777@gmail.com>
2025-12-15 18:45:21 +0100

    Implement file organization and folder creation
    Added functionality to split files into categories based on their extensions and create corresponding folders in the Downloads directory.

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ import os
 import time 
 import rich
 
+def split_files():
+    files = os.listdir("Donwloads")
+
+    all_ext = [
+        # Images
+        [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".svg", ".ico"],
+
+        # Videos
+        [".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv", ".webm", ".mpeg", ".mpg"],
+
+        # Music / Audio
+        [".mp3", ".wav", ".aac", ".flac", ".ogg", ".m4a", ".wma"],
+
+        # Documents
+        [".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".txt", ".csv", ".md", ".rtf"]
+    ]
+
+    for file in files:
+        name, ext = os.path.splitext(file)
+
+        if ext in all_ext:
+            print(file)
+    
 
 
 def create_folders():
@@ -10,11 +33,23 @@ def create_folders():
     choice = input("[Tool]: Can I Create Them? (y/n): ").lower()
 
     if choice == "y" or choice == "yes":
-        pass
+        if not os.path.exists("Downloads/Images"):
+            os.makedirs("Downloads/Images")
+            print("[+] - Images Folder Created")
+        if not os.path.exists("Downloads/Videos"):
+            os.makedirs("Downloads/Videos")
+            print("[+] - Videos Folder Created")
+        if not os.path.exists("Downloads/Music"):
+            os.makedirs("Downloads/Music")
+            print("[+] - Music Folder Created")
+        if not os.path.exists("Downloads/Documents"):
+            os.makedirs("Downloads/Documents")
+            print("[+] - Documents Folder Created")
+
     elif choice == "n" or choice == "no":
-        pass
+        print("[Tool]: Why Even You Opend Me ???")
     else:
-        print("[Tool]: Try With (yes or no)")
+        print("[Tool]: ... Try With (yes or no)")
     
 
 def main():
@@ -22,8 +57,11 @@ def main():
     choice = input("[Tool]: Do You Want Me To Organiez Your Donlowdes Folder ? (y/n): ").lower()
     
     if choice == "y" or choice == "yes":
-        print("[Tool]: Cool")
+        create_folders()
+        split_files()
     elif choice == "n" or choice == "no":
         print("[Tool]: What a wierdo")
     else:
         print(f"[Tool]: For Real Bro ? You had One Job, Try (Yes OR No)")
+
+main()
